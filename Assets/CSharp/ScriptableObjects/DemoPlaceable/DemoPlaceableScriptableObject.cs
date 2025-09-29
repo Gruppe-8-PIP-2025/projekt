f83e2a703e38c255ecbe53e5b76cd1d3d85e20df
Maria Lindling <maria.neith.lindling@protonmail.com>
2025-09-29 12:44:30 +0200

    set up demo scene mk2

--- Assets/CSharp/ScriptableObjects/DemoPlaceable/DemoPlaceableScriptableObject.cs
+++ Assets/CSharp/ScriptableObjects/DemoPlaceable/DemoPlaceableScriptableObject.cs
@@ -5,6 +5,8 @@ public class DemoPlaceableScriptableObject : ScriptableObject
 {
   [SerializeField] private string localizedName;
   [SerializeField] private GameObject asset;
+  [SerializeField] private Vector3 positionAdjustment;
+  [SerializeField] private Quaternion rotationAdjustment;
   [SerializeField] private Vector2 gridTile;
 
   public string LocalizedName => localizedName;
@@ -13,8 +15,26 @@ public class DemoPlaceableScriptableObject : ScriptableObject
 
   public GameObject GameObject { get; set; }
 
+  public void ApplyPosition()
+  {
+    GameObject.transform.position += positionAdjustment;
+  }
+
+  public void ApplyRotation()
+  {
+    GameObject.transform.rotation *= rotationAdjustment;
+  }
+
   public void SetGridTile(GridTile position)
   {
     gridTile = position.Position;
+  }
+
+  public void InstantiateTo(GridTile position)
+  {
+    SetGridTile(position);
+    GameObject.transform.SetParent(position.gameObject.transform);
+    ApplyPosition();
+    ApplyRotation();
   }
 }
